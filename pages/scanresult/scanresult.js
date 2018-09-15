fd149f85020f9fc44a86d209ab95899ec828ad50
Seiei-Leung <786883603@qq.com>
2018-09-15 12:00:19 +0800

    修改入仓顺序逻辑

--- pages/scanresult/scanresult.js
+++ pages/scanresult/scanresult.js
@@ -8,14 +8,12 @@ Page({
    * 页面的初始数据
    */
   data: {
-    activeIndex: 0,
     storageNum: "",
-    orderno: "",
-    bedNum: "",
-    size: "",
     dialogTitle: "",
     dialogTxt: "",
-    iconType: 0
+    iconType: 0,
+    resultList: [],
+    kind: ""
   },
   onReady: function () {
     this.dialog = this.selectComponent("#dialog");
@@ -36,43 +34,53 @@ Page({
         },
         success: function (res) {
           that.setData({
-            storageNum: options.storageNum
+            storageNum: options.storageNum,
+            resultList: res.data,
+            kind: res.data[0] ? res.data[0].kind : ""
           })
-          if (res.data[0] && res.data[0].kind == "分床") {
-            that.setData({
-              activeIndex: 0,
-              orderno: res.data[0].orderno,
-              bedNum: res.data[0].bednno
-            })
-          } else if (res.data[0] && res.data[0].kind == "分码") {
-            that.setData({
-              activeIndex: 1,
-              orderno: res.data[0].orderno,
-              size: res.data[0].sizes
-            })
-          }
         }
       })
     }
   },
   scanForBedNum: function (event) {
+    if (this.data.resultList.length == 2) {
+      this.setData({
+        dialogTitle: "提示",
+        dialogTxt: "仓位放置裁片限为两床次或两码的货物！",
+        iconType: 0
+      });
+      this.dialog.showModal();
+      return;
+    }
     var
       that = this,
       getUrl,
       kind = event.currentTarget.dataset.kind;
     
     if (kind == "bed") {
+      if (this.data.kind == '分码') {
+        this.setData({
+          dialogTitle: "提示",
+          dialogTxt: "该仓位为分码，不能放分床裁片",
+          iconType: 0
+        });
+        this.dialog.showModal();
+        return;
+      }
       kind = "分床";
       getUrl = app.globalData.twUrl + "/estapi/api/CutPieceEntry/InsertData";
-      that.setData({
-        activeIndex: 0
-      });
     } else {
+      if (this.data.kind == '分床') {
+        this.setData({
+          dialogTitle: "提示",
+          dialogTxt: "该仓位为分床，不能放分码裁片",
+          iconType: 0
+        });
+        this.dialog.showModal();
+        return;
+      }
       kind = "分码";
       getUrl = app.globalData.twUrl + "/estapi/api/CutPieceEntry/InsertMatchedData";
-      that.setData({
-        activeIndex: 1
-      });
     }
     wx.scanCode({
       onlyFromCamera: true,
@@ -106,12 +114,16 @@ Page({
             },
             complete: function (res) {
               if (res.data.result == "SUCCESS") {
+                var obj = {};
+                obj.orderno = sendData.orderno;
+                obj.bednno = sendData.bedno;
+                obj.sizes = sendData.sizes;
+                obj.kind = sendData.kind;
+                var list = that.data.resultList.slice(0, 2);
+                list.push(obj);
                 that.setData({
-                  orderno: sendData.orderno,
-                  bedNum: sendData.bedno,
-                  size: sendData.sizes
-                });
-                that.setData({
+                  kind: sendData.kind,
+                  resultList: list,
                   dialogTitle: "成功",
                   dialogTxt: "扫码成功",
                   iconType: 1
@@ -131,7 +143,16 @@ Page({
       }
     });
   },
-  inputByHand: function() {
+  inputByHand: function () {
+    if (this.data.resultList.length == 2) {
+      this.setData({
+        dialogTitle: "提示",
+        dialogTxt: "仓位放置裁片限为两床次或两码的货物！",
+        iconType: 0
+      });
+      this.dialog.showModal();
+      return;
+    }
     wx.navigateTo({
       url: '../inputbyhand/inputbyhand?storageNum=' + this.data.storageNum
     })
